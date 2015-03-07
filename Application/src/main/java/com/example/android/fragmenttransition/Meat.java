3316f7d7927de54b35caec6498ebe5ab75df6b94
maribel.garcia <maribel.garcia@televisatim.com>
2015-03-06 18:20:15 -0600

    Primera version 2

--- Application/src/main/java/com/example/android/fragmenttransition/Meat.java
+++ Application/src/main/java/com/example/android/fragmenttransition/Meat.java
@@ -22,7 +22,7 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 /**
- * This represents a sample data.
+ * This represents a sample data.mm
  */
 public class Meat {
 
